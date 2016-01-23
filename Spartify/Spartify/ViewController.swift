74643f632c895afa6a4a937cf2f63493302fd051
Eunice Tse <Eunice@SEAS345.wlan.seas.upenn.edu>
2016-01-22 23:19:00 -0500

    add parse login

--- Spartify/Spartify/ViewController.swift
+++ Spartify/Spartify/ViewController.swift
@@ -7,15 +7,25 @@
 //
 
 import UIKit
+import ParseUI
 import Parse
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,PFLogInViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        if (PFUser.currentUser() == nil) {
+            let loginViewController = PFLogInViewController()
+            loginViewController.delegate = self
+            self.presentViewController(loginViewController, animated: false, completion: nil)
+        }
     }
 
     override func didReceiveMemoryWarning() {
